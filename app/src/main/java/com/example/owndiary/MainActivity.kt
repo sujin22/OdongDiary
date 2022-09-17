e728eaf51fb7a64339e10c33ec30d1ac878ba7fd
sujin22 <aoghksj@naver.com>
2022-09-17 23:16:27 +0900

    MainActivity - compose Image, Card, State

--- app/src/main/java/com/example/owndiary/MainActivity.kt
+++ app/src/main/java/com/example/owndiary/MainActivity.kt
@@ -3,38 +3,87 @@ package com.example.owndiary
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.owndiary.ui.theme.OwnDiaryTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            OwnDiaryTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+            var isFavorite by rememberSaveable{
+                mutableStateOf(false) //기본값으로 false지정
+            }
+            ImageCard(
+                modifier = Modifier
+                    .fillMaxWidth(0.5f) //전체의 절반 차지
+                    .padding(16.dp),
+                isFavorite = isFavorite
+            ){ favorite ->
+                isFavorite = favorite  //callback 내용
             }
         }
+
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    OwnDiaryTheme {
-        Greeting("Android")
+fun ImageCard(
+    modifier: Modifier = Modifier, //기본값 지정
+    isFavorite: Boolean,
+    onTabFavorite:(Boolean) -> Unit,//Callback 생성
+) {
+    Card(
+        modifier = modifier,
+        shape = RoundedCornerShape(8.dp),
+        elevation = 5.dp,
+    ) {
+        Box(
+            modifier = Modifier.height(200.dp)
+        ) {
+            Image(
+                painterResource(id = R.drawable.poster),
+                contentDescription = "poster",
+                contentScale = ContentScale.Crop,
+            )
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.TopEnd,
+            ){
+                IconButton(onClick = {
+                    //onTabFavorite.invoke(!isFavorite) //invoke는 생략 가능하다.
+                    onTabFavorite(!isFavorite)
+                }) {
+                    Icon(
+                        imageVector = if(isFavorite) Icons.Default.Favorite
+                                        else Icons.Default.FavoriteBorder,
+                        contentDescription = "favorite",
+                        tint = Color.White)
+                }
+            }
+        }
     }
 }
