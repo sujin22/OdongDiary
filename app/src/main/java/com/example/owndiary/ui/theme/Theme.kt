81c014ca662d6617d77cd3ece4afeb12be772529
sujin22 <aoghksj@naver.com>
2022-10-09 12:09:53 +0900

    feat: status bar color

--- app/src/main/java/com/example/owndiary/ui/theme/Theme.kt
+++ app/src/main/java/com/example/owndiary/ui/theme/Theme.kt
@@ -6,6 +6,7 @@ import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 private val DarkColorPalette = darkColors(
         primary = Color.Black,
@@ -34,6 +35,16 @@ fun OwnDiaryTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composab
         DarkColorPalette
     } else {
         LightColorPalette
+    }
+    val systemUiController = rememberSystemUiController()
+    if(darkTheme){
+        systemUiController.setSystemBarsColor(
+            color = Color.Transparent
+        )
+    }else{
+        systemUiController.setSystemBarsColor(
+            color = Color.White
+        )
     }
 
     MaterialTheme(
